55487aec86b4386f5840ede99f57f7924bdc1d98
NinaA <averill.nina@gmail.com>
2019-04-27 23:54:39 +1000

    Refactored comments

--- a_star.py
+++ a_star.py
@@ -11,7 +11,6 @@ class AStar:
     @param goal_hexs    : dictionary mapping coordinates to all goal hexs for the current player
     @param obstacles    : list of types for all obstacles on the board
     """
-    # initialise the algorithm with the board, starting positions, goals and blocks
     def __init__(self, board_dict, start_hexs, goal_hexs, obstacles):
         self._empty_board = board_dict
         self._start = start_hexs
@@ -47,31 +46,28 @@ class AStar:
             # get the valid neighbouring tiles
             children = curr_move.get_children(board_state, self._obstacles)
 
-            # loop through all valid neighbours
             for child in children:
-                if child in explored:  # check if node hasn't already been expanded
+                # check if node hasn't already been expanded
+                if child in explored:
                     continue
 
-                child.set_g(curr_move.g() + child.cost)  # g cost increases from parent based on uniform value
-                child.set_h()  # calculate heuristic cost
+                child.set_g(curr_move.g() + child.cost)
+                child.set_h()
 
-                if child in frontier:  # check if neighbour already in unexplored
-                    if child.g() > frontier.get(child).g():  # check if existing neighbour already
-                        continue                             # has lower g cost
+                if child in frontier:
+                    # if equivalent explored node has lower path cost, ignore current node
+                    if child.g() > frontier.get(child).g():
+                        continue
                 # add low g cost, unexplored neighbour
                 frontier.put(child, child.f())
 
         path = []
-        current = curr_move  # get current state of the board
-        while current is not None:  # loop until a node has a null parent i.e. start node
-            # print("f:", current.f())
-            # print("g:", current.g())
-            # print("h:", current.h())
-            # print()
-            path.append(current)  # add node to final path
+        current = curr_move
+        while current is not None:
+            path.append(current)
             current = current.parent()
-
-        return path[::-1]  # reverse path
+        # reverse path
+        return path[::-1]
 
     """
     Method to update empty board dict with current player piece positions
@@ -81,7 +77,7 @@ class AStar:
     def update_board(self, state):
         board = {key: value.copy() for key, value in self._empty_board.items()}
 
-        for hex in state:  # change status of board to map every piece accurately
+        for hex in state:
             board[hex.get_coordinate()] = hex
 
         return board
